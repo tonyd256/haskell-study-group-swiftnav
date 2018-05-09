576f170e01a6a29d7ef1f754022b722ce35efeb3
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2018-05-09 16:42:33 -0700

    Request RTM URL from Slack API
    This is the result of the first coding session in the study group. We
    ask Slack for a connection to RTM and print the URL to the terminal.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( app
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.IO
+import System.Environment
+import qualified Configuration.Dotenv as Dotenv
+import Configuration.Dotenv.Types (defaultConfig)
+import Network.Wreq
+import qualified Data.Text as T
+import Control.Lens ((&), (^?), (.~))
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import Data.Aeson.Lens
+
+app :: IO ()
+app = do
+  hSetBuffering stdout NoBuffering
+  Dotenv.loadFile defaultConfig
+
+  token <- T.pack <$> getEnv "SLACK_API_TOKEN"
+
+  let opts = defaults & param "token" .~ [token]
+  r <- getWith opts "https://slack.com/api/rtm.connect"
+  print $ r ^? responseBody . key "url" . _String
